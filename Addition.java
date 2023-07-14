fb93350be0534fe6ba1a9f9c135552598fef6a5b
monisha785 <monisha932001@gmail.com>
2023-07-14 15:16:35 +0530

    commtting program

---
+++ Addition.java
@@ -0,0 +1,33 @@
+class Addition{
+public static void main(String addition[]){
+  add(56,98);
+  add(67,34);
+  add(127,987);
+  add(34,54);
+  add(32,760);
+  add(145,908);
+  add(254,130);
+  add(66,88);
+  add(66,34);
+  add(132,547);
+
+ add(46,90,34);
+ add(69,64,67);
+ add(167,997,78);
+ add(345,546,90);
+ add(3,76,800);
+ add(45,98,89);
+ add(25,10,901);
+ add(66,88,78);
+ add(678,348,863);
+ add(12,768,72);
+ }
+ 
+ public static void add(int z , int u) {     
+ System.out.println(z+u);
+ }
+ public static void add(int z , int u , int y) {     
+ System.out.println(z+u+y);
+ }
+}
+  
