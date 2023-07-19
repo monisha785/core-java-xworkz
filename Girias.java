364332c02dae161d39714c42b7aadf1480f392a3
monisha785 <139444305+monisha785@users.noreply.github.com>
2023-07-19 14:12:31 +0530

    Add files via upload

---
+++ Girias.java
@@ -0,0 +1,10 @@
+class Girias{
+public static void main(String arg[]){
+System.out.println("Main started");
+String electronicDevices[]={"refrigerator" , "washing machine" , "television" , "led" ,  "microwave" ,"grinder","ac","ironbox"};
+ for(int devsIndex = 0; devsIndex < electronicDevices.length; devsIndex++ ){
+ System.out.println(electronicDevices[devsIndex]);
+}
+System.out.println("Main ended");
+}
+}
